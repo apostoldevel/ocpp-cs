94f5568b2e3fee5717ed67c3592897c1daf44ac0
Преподобный Ален <alienufo@inbox.ru>
2023-02-09 21:08:25 +0300

    Updated web shell.

--- www/config.js
+++ www/config.js
@@ -1,24 +1,30 @@
 var appConfig = {
   defaultLanguage: "en",
 
-  creditsText: "OCPP Central System © 2022",
-  creditsShortText: "OOCP CS",
+  creditsText: "OCPP Central System © 2023",
+  creditsShortText: "OOCP CSS",
 
   signIn: "/signin",
   signUp: "/signup",
 
   confAuthorize: true,
   confCrm: false,
+  confDriver: false,
   confOcpp: true,
   confAdmin: false,
+  googleAuthorize: false,
+
+  mapLanguage: "en_US",
+
+  googleClientId: null,
 
   ocppApiDomain: "http://localhost:9220",
   ocppApiPath: "/api/v1",
   ocppApiClienId: "web-ocpp-css.com",
 
-  apiTokenUrl: "/oauth2/token",
-  apiDomain: "http://localhost:4977",
-  wsDomain: "ws://localhost:4977",
+  apiTokenUrl: "http://localhost:8080/oauth2/token",
+  apiDomain: "http://localhost:8080",
+  wsDomain: "ws://localhost:8080",
   apiPath: "/api/v1",
   apiClientId: "web-ocpp-css.com",
 
@@ -36,5 +42,5 @@ var appConfig = {
     network: {},
   },
 
-  maxFileSize: 512000,
+  maxFileSize: 512000
 };
